47e83f35e9f7fe441f13d4bc774348992a269aa7
zealptl <zealpatel06@gmail.com>
2020-06-19 18:34:42 -0400

    Added password hashing

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 
 const User = require('../models/User');
@@ -17,14 +18,33 @@ router.post(
       'Please enter a password with six or more characters'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('passed');
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) return res.status(400).json({ msg: 'User already exists ' });
+
+      user = new User({ name, email, password });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send('User saved');
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 
