07c1780afe02c377d1f21c810c66c82f727d459a
zealptl <zealpatel06@gmail.com>
2020-06-22 11:58:26 -0400

    Setup Auth context

---
+++ client/src/context/auth/AuthState.js
@@ -0,0 +1,50 @@
+import React, { useReducer } from 'react';
+import AuthContext from './AuthContext';
+import AuthReducer from './AuthReducer';
+import {
+  REGISTER_SUCCESS,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  CLEAR_ERRORS,
+} from '../types';
+
+const AuthState = (props) => {
+  const intialState = {
+    token: localStorage.getItem('token'),
+    isAuthenticated: null,
+    loading: true,
+    user: null,
+    error: null,
+  };
+
+  const [state, dispatch] = useReducer(AuthReducer, intialState);
+
+  // Load User
+
+  // Register User
+
+  // Login User
+
+  // Logout User
+
+  // Clear Errors
+
+  return (
+    <AuthContext.Provider
+      value={{
+        token: state.token,
+        isAuthenticated: state.isAuthenticated,
+        loading: state.loading,
+        user: state.user,
+        error: state.error,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthState;
